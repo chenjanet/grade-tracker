c0fa01e637e96e301a7b179ba332a5ba16dd4321
j985chen <j985chen@uwaterloo.ca>
2021-05-11 23:07:05 -0400

    added +New Group component

---
+++ src/components/newGroupAdder.component.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './components.css';
+
+export default function NewGroupAdder() {
+    return (
+        <div className="card pb-3 pt-2 pl-2 pr-2 newGroupAdder">
+            <h3>+ Add group</h3>
+        </div>
+    )
+}

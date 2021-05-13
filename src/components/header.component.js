71a860cfeeab5777e8f46839e2a3af5b7b791745
j985chen <j985chen@uwaterloo.ca>
2021-05-12 22:01:28 -0400

    more changes to component styling

--- src/components/header.component.js
+++ src/components/header.component.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Header(props) {
     return (
         <div className='pt-1 pb-1 pl-2 pr-2 header-wrapper'>
-            <h3>{props.text}</h3>
+            <h2>{props.text}</h2>
         </div>
     )
 }
--- src/components/newGroupAdder.component.js
+++ src/components/newGroupAdder.component.js
@@ -5,7 +5,7 @@ import './components.css';
 export default function NewGroupAdder() {
     return (
         <div className="card pb-3 pt-2 pl-2 pr-2 newGroupAdder">
-            <h3>+ Add group</h3>
+            <h3>+ Add term</h3>
         </div>
     )
 }

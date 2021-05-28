9ce72c4e8c63e3cdeea5c33c0497236db4b95ae3
j985chen <j985chen@uwaterloo.ca>
2021-05-27 23:54:06 -0400

    added settings button component

---
+++ src/components/settingsButton.component.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Deleter from './deleter.component';
+import Renamer from './renamer.component';
+
+export default function SettingsButton({ componentName, componentId, deleteComponent, renameComponent }) {
+    const [optionsShow, setOptionsShow] = useState(false);
+
+    const className = (optionsShow) ? 'settingsOptions visible' : 'settingsOptions';
+    
+    return (
+        <>
+            <button 
+                className='settingsButton' 
+                onMouseEnter={() => setOptionsShow(true)}
+                onMouseLeave={() => setOptionsShow(false)}
+            >
+                <FontAwesomeIcon icon='cog' />
+            </button>
+            <div 
+                className={className}
+                onMouseEnter={() => setOptionsShow(true)}
+                onMouseLeave={() => setOptionsShow(false)}
+            >
+                <Renamer 
+                    initialName={componentName} 
+                    renameId={componentId} 
+                    renameComponent={renameComponent} 
+                />
+                <Deleter 
+                    deleteId={componentId} 
+                    deleteComponent={deleteComponent} 
+                />
+            </div>
+        </>
+    )
+}
